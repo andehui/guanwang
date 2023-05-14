560a90ff16e8bce3ae9d91b6316d6566f2ae77d5
andehui <87691830+andehui@users.noreply.github.com>
2023-05-14 22:22:58 +0800

    Add files via upload

---
+++ api-keys/key.php
@@ -0,0 +1,30 @@
+
+
+
+
+<br>
+<br>
+<br>
+<b>Chat key:</>
+<br>
+<hr style=" height:2px;border:none;border-top:2px dotted #185598;" />
+<br>
+
+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
+<br>
+<hr style=" height:2px;border:none;border-top:2px dotted #185598;" />
+
+<br><b>Do not share with others</b>
+
+<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1805477885216561"
+     crossorigin="anonymous"></script>
+<!-- 免费keyapi -->
+<ins class="adsbygoogle"
+     style="display:block"
+     data-ad-client="ca-pub-1805477885216561"
+     data-ad-slot="5340916772"
+     data-ad-format="auto"
+     data-full-width-responsive="true"></ins>
+<script>
+     (adsbygoogle = window.adsbygoogle || []).push({});
+</script>
